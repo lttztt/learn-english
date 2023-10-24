f0da7ccbf3ae0084e6094fbd602f9704e32b9ef5
liuzhe <liuzhexx@foxmail.com>
2023-10-24 23:27:59 +0800

    fix：消除因为while (textList[i + 1] 的1写成i引发bug

--- scripts/parsePdf.js
+++ scripts/parsePdf.js
@@ -6,7 +6,6 @@ function main() {
 
   pdf(dataBuffer).then(function (data) {
     const result = parse(data.text)
-    // fs.writeFileSync('./01-raw.txt', data.text)
     fs.writeFileSync('./01.json', JSON.stringify(result))
   })
 }
@@ -23,7 +22,6 @@ function parse(text) {
   //   - 空的
   //   - 数字开头的
   const textList = rawTextList.slice(startIndex + 1).filter(t => t && !/\d/.test(Number(t)))
-  console.log(textList);
   // 3. 每两个一组
   const result = []
 
@@ -36,52 +34,53 @@ function parse(text) {
 
     function run() {
       const element = textList[i];
-      let chinese = ''
-      let englishAndSoundMark = ''
+      let chinese = "";
+      let englishAndSoundMark = "";
+
       if (isChinese(element)) {
-        chinese += element
+        chinese += element;
         while (isChinese(textList[i + 1])) {
-          chinese += '，' + textList[i + 1]
+          chinese += "，" + textList[i + 1];
           i++;
         }
 
-        data.chinese = chinese
+        data.chinese = chinese;
       } else {
-        englishAndSoundMark += element
-        while (textList[i + i] && !isChinese(textList[i + 1])) {
-          englishAndSoundMark += ' ' + textList[i + 1]
+        englishAndSoundMark += element;
+        while (textList[i + 1] && !isChinese(textList[i + 1])) {
+          englishAndSoundMark += " " + textList[i + 1];
           i++;
         }
 
-        const { english, soundMark } = parseEnglishAndSoundMark(englishAndSoundMark)
-        data.english = english
-        data.soundMark = soundMark
+        const { english, soundMark } = parseEnglishAndSoundMark(englishAndSoundMark);
+        data.english = english;
+        data.soundMark = soundMark;
       }
     }
 
-    run()
-    i++
-    run()
+    run();
+    console.log(data);
+    i++;
+    run();
     result.push(data)
   }
-  console.log(result);
   return result
 }
 
 function isChinese(str) {
-  var pattern = /^[\u4E00-\u9FA5]/; // 中文字符的Unicode范围
-  return pattern.test(str);
+  const reg = /^[\u4e00-\u9fa5]/;
+  return reg.test(str);
 }
 
-
-// 根据英文和音标，解析出英文和音标 以 / 分割
 function parseEnglishAndSoundMark(text) {
-  const list = text.split(' ')
-  const soundMarkStartIndex = list.findIndex(t => t.startsWith('/'))
-  const english = list.slice(0, soundMarkStartIndex).join(' ')
-  const soundMark = list.slice(soundMarkStartIndex).join(' ')
+  const list = text.split(" ");
+  const soundMarkdStartIndex = list.findIndex((t) => t.startsWith("/"));
+
+  const english = list.slice(0, soundMarkdStartIndex).join(" ");
+  const soundMark = list.slice(soundMarkdStartIndex).join(" ");
+
   return {
     english,
     soundMark,
-  }
+  };
 }
